97dded15a81817e3f43c9eeb513c7a0f5a5eb45d
Ruchitha Bindhu <Ruchitha.Bindhu@triconinfotech.com>
2024-01-02 10:46:21 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { ProductsComponent } from './products/products.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: ' ', component: AppComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'products', component: ProductsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
